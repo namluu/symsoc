88fa44156a721277eb86891c909c7e0f47e899a1
Nam Luu <nam.luuduc@gmail.com>
2020-12-06 00:37:48 +0700

    init template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
      */
     public function homepage()
     {
-        return new Response('hello Nam');
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
